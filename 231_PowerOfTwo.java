602122984601cbb678e2e1dd080d430ab9e58714
Justin <39439980+Justin6842@users.noreply.github.com>
2023-05-05 18:30:54 -0400

    Add files via upload

---
+++ 231_PowerOfTwo.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        for (int i = 0; i < 31; i++){
+            if (Math.pow(2, i) == n) return true;
+        }
+        return false;
+    }
+}
