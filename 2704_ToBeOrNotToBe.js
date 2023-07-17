6c149eda702878b0da6eb2a45fd5c65c52b8bc4b
Justin <39439980+Justin6842@users.noreply.github.com>
2023-07-16 20:24:54 -0400

    Add files via upload

---
+++ 2704_ToBeOrNotToBe.js
@@ -0,0 +1,12 @@
+var expect = function(val) {
+    return {
+        toBe: (val2) => {
+            if (val !== val2) throw new Error("Not Equal");
+            return true;
+        },
+        notToBe: (val2) => {
+            if (val === val2) throw new Error("Equal");
+            return true;
+        }
+    }
+};
