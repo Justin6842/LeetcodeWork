32dcf8968359cbfe2dcaf7f19413c86798197334
unknown <justin6842@gmail.com>
2024-06-14 02:55:32 -0400

    daily 6-14

---
+++ Leetcode_2024/945.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int minIncrementForUnique(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int sol = 0;
+        for (int i = 1; i < nums.size(); i++){
+            if (nums[i] <= nums[i-1]){
+                sol += (nums[i-1]+1-nums[i]);
+                nums[i] = nums[i-1]+1;
+            }
+        }
+        return sol;
+    }
+};
